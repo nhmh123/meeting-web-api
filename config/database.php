ec38c26ba4460a1f71744a236afbfd2006d49f1c
nhmh <dh52105753@gmail.com>
2025-04-11 15:57:52 +0700

    update db config

--- config/database.php
+++ config/database.php
@@ -103,6 +103,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
+            'host' => parse_url(env('DATABASE_URL'), PHP_URL_HOST),
+            'port' => env('DB_PORT', '5432'),
+            'database' => ltrim(parse_url(env('DATABASE_URL'), PHP_URL_PATH), '/'),
+            'username' => parse_url(env('DATABASE_URL'), PHP_URL_USER),
+            'password' => parse_url(env('DATABASE_URL'), PHP_URL_PASS),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
